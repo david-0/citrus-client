9e11680ee2fb29df3c40d48354957e1f8e2ec99f
David Leuenberger <david.leuenberger@hsr.ch>
2017-08-25 21:06:42 +0300

    use own theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MdToolbarModule } from '@angular/material';
 import 'hammerjs';
@@ -17,6 +18,7 @@ import { AppRouteModule } from './router/app-route.module';
     BrowserModule,
     AppRouteModule,
     MdToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
