17c61ed2a7fd93df42b142c213f3c62a3cd04ffe
David Leuenberger <david.leuenberger@hsr.ch>
2017-09-09 13:40:44 +0300

    rename component and folder

--- src/app/router/app-route.module.ts
+++ src/app/router/app-route.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from '../dashboard/dashboard.component';
-import {TransportationOverviewComponent} from '../childs/transportation/transportation-overview/transportation-overview.component';
+import {TransportationOverviewComponent} from '../childs/transport/transport-overview/transportation-overview.component';
 import {AdministrationComponent} from '../childs/administration/administration.component';
 import {BuyComponent} from '../childs/buy/buy.component';
 import {FetchComponent} from '../childs/fetch/fetch.component';
-import {CreateTransportComponent} from '../childs/transportation/create-transport/create-transport.component';
+import {CreateTransportComponent} from '../childs/transport/create-transport/create-transport.component';
 import {StorageOverviewComponent} from '../childs/storage/storage-overview/storage-overview.component';
 import {UsersOverviewComponent} from '../childs/users/users-overview/users-overview.component';
 import {FruitsOverviewComponent} from '../childs/fruits/fruits-overview/fruits-overview.component';
