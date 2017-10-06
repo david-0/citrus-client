a60b99b38b6974ca2cca24f00dfa9a3f285101a7
David Leuenberger <david.leuenberger@hsr.ch>
2017-10-06 07:24:23 +0200

    use table-filter in transport overview

--- src/app/childs/transport/transport-overview/transport-overview.component.ts
+++ src/app/childs/transport/transport-overview/transport-overview.component.ts
@@ -1,14 +1,11 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {GenericDatabase} from '../../../list-support/generic-database';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ITransport} from '../../../entities/ITransport';
-import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import {GenericPagedDataSource} from '../../../list-support/generic-paged-data-source';
 import {MdPaginator, MdSort} from '@angular/material';
-import {TransportTO} from '../../../TransferObjects/TransportTO';
 import {TransportDatabaseService} from '../transport-database.service';
 import {TransportSettingsService} from '../transport-settings.service';
 
@@ -22,30 +19,24 @@ export class TransportOverviewComponent implements OnInit {
 
   public dataSource: GenericPagedDataSource<ITransport> | null;
 
-  constructor(private database: TransportDatabaseService, private settings: TransportSettingsService) {
+  constructor(private database: TransportDatabaseService, public settings: TransportSettingsService) {
   }
 
-  @ViewChild('filter') filter: ElementRef;
   @ViewChild(MdPaginator) paginator: MdPaginator;
   @ViewChild(MdSort) sort: MdSort;
 
   ngOnInit() {
-    this.filter.nativeElement.value = this.settings.filterValue;
     this.dataSource = new GenericPagedDataSource(this.database, this.paginator, this.sort, this.settings);
-    this.dataSource.filter = this.filter.nativeElement.value;
-    Observable.fromEvent(this.filter.nativeElement, 'keyup')
-      .debounceTime(150)
-      .distinctUntilChanged()
-      .subscribe(() => {
-        if (!this.dataSource) {
-          return;
-        }
-        this.dataSource.filter = this.filter.nativeElement.value;
-        this.settings.filterValue = this.filter.nativeElement.value;
-      });
     this.paginator.page.subscribe(event => {
       this.settings.pageIndex = event.pageIndex;
       this.settings.pageSize = event.pageSize;
     });
+  }
+
+  public onFilterChange(filter: string) {
+    if (!this.dataSource) {
+      return;
+    }
+    this.dataSource.filter = filter;
   }
 }
