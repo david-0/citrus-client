371f866edfe40b1c99188e156443cfd01572c35e
David Leuenberger <david.leuenberger@hsr.ch>
2017-11-30 22:02:38 +0100

    use progress-spinner in address overview

--- src/app/childs/address/address-overview/address-overview.component.ts
+++ src/app/childs/address/address-overview/address-overview.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from "@angular/core";
 import {MatPaginator, MatSort} from "@angular/material";
 import {IAddress} from "citrus-common";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 import {GenericPagedDataSource} from "../../../table-support/generic-paged-data-source";
 import {AddressRestService} from "../address-rest.service";
 import {AddressSettingsService} from "../address-settings.service";
@@ -12,6 +13,7 @@ import {AddressSettingsService} from "../address-settings.service";
 })
 export class AddressOverviewComponent implements OnInit {
   public displayedColumns = ["name", "prename", "street", "number", "plz", "city"];
+  public loading = new BehaviorSubject<boolean>(false);
 
   public dataSource: GenericPagedDataSource<IAddress> | null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -21,7 +23,7 @@ export class AddressOverviewComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.dataSource = new GenericPagedDataSource(this.database, this.paginator, this.sort, this.settings);
+    this.dataSource = new GenericPagedDataSource(this.database, this.paginator, this.sort, this.settings, this.loading);
     this.paginator.page.subscribe(event => {
       this.settings.pageIndex = event.pageIndex;
       this.settings.pageSize = event.pageSize;
