73d82de8cfff81071078070200354ba857e1e74c
David Leuenberger <david.leuenberger@hsr.ch>
2017-12-01 16:35:46 +0100

    remove unused code

--- src/app/table-support/generic-rest.service.ts
+++ src/app/table-support/generic-rest.service.ts
@@ -6,10 +6,7 @@ import {RangeResult} from "./range-result";
 export class GenericRestService<T extends IId> {
   private headers = new HttpHeaders({"Content-Type": "application/json"});
 
-  private i = 2;
-
   constructor(private http: HttpClient, private restUrl: string) {
-    this.i = 3;
   }
 
   add(item: T): Observable<T> {
