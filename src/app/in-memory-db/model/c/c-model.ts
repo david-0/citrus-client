9c72c340842787fe9242c58d3ef8211ccd5e340c
David Leuenberger <david.leuenberger@hsr.ch>
2018-01-11 19:28:31 +0100

    use readonly instead of a getter only in CModel

--- src/app/in-memory-db/model/c/c-model.ts
+++ src/app/in-memory-db/model/c/c-model.ts
@@ -2,12 +2,8 @@ export class CModel {
   private _lastRead?: Date;
   private _lastWrite?: Date;
 
-  constructor(private _id: number) {
+  constructor(public readonly id: number) {
     this._lastWrite = new Date();
     this._lastRead = new Date();
-  }
-
-  public get id() {
-    return this._id;
   }
 }
