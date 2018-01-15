e79ddaabbcee8f4ddfdad4c238771c0d404e6d72
David Leuenberger <david.leuenberger@hsr.ch>
2018-01-15 18:17:46 +0100

    use readonly instead of private and getter

--- src/app/in-memory-db/projector/abstract-projector.ts
+++ src/app/in-memory-db/projector/abstract-projector.ts
@@ -2,20 +2,12 @@ import {Caches} from "../cache/caches";
 import {Projectors} from "./projectors";
 
 export abstract class AbstractProjector {
-  constructor(private _caches: Caches, private _projectors: Projectors) {
+  constructor(protected readonly caches: Caches, protected readonly projectors: Projectors) {
   }
 
   public abstract projectOneAndUpdateCache(tItem: any): any;
 
   public projectManyAndUpdateCache(tItems: any[]): any[] {
     return tItems.map(tItem => this.projectOneAndUpdateCache(tItem));
-  }
-
-  protected get caches() {
-    return this._caches;
-  }
-
-  protected  get projectors() {
-    return this._projectors;
   }
 }
