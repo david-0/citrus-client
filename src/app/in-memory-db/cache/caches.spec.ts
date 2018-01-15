67e7acde296dfd60c647ece2aa798ca4241ce8ad
David Leuenberger <david.leuenberger@hsr.ch>
2018-01-15 20:59:36 +0100

    rewrite test with spy's

--- src/app/in-memory-db/cache/caches.spec.ts
+++ src/app/in-memory-db/cache/caches.spec.ts
@@ -1,26 +1,21 @@
-import {CUser} from "../model/c/c-user";
 import {Caches} from "./caches";
-import {CAddress} from "../model/c/c-address";
-import {TypeCache} from "./type-cache";
+import createSpyObj = jasmine.createSpyObj;
 
 describe("Caches", () => {
   it("should be able to add and get", () => {
     const caches = new Caches();
-    const typeCache = new TypeCache<CAddress>();
-    typeCache.synchronizeOne(new CAddress(12));
-    caches.addCache("Address", typeCache);
-    expect(caches.getCache("Address")).toBe(typeCache);
+    const addressCacheSpy = createSpyObj("CAddressSpy", ["dummy"]);
+    caches.addCache("Address", addressCacheSpy);
+    expect(caches.getCache("Address")).toBe(addressCacheSpy);
   });
 
   it("should be able to add and get multiple caches", () => {
     const caches = new Caches();
-    const typeCacheAddress = new TypeCache<CAddress>();
-    typeCacheAddress.synchronizeOne(new CAddress(12));
-    const typeCacheUser = new TypeCache<CUser>();
-    typeCacheUser.synchronizeOne(new CUser(11));
-    caches.addCache("Address", typeCacheAddress);
-    caches.addCache("User", typeCacheUser);
-    expect(caches.getCache("Address")).toBe(typeCacheAddress);
-    expect(caches.getCache("User")).toBe(typeCacheUser);
+    const addressCacheSpy = createSpyObj("CAddressSpy", ["dummy"]);
+    const userCacheSpy = createSpyObj("CUserSpy", ["dummy"]);
+    caches.addCache("Address", addressCacheSpy);
+    caches.addCache("User", userCacheSpy);
+    expect(caches.getCache("Address")).toBe(addressCacheSpy);
+    expect(caches.getCache("User")).toBe(userCacheSpy);
   });
 });
