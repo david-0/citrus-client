c6e4a62110bb85ccc13c9551feddc571163b752c
David Leuenberger <david.leuenberger@hsr.ch>
2018-01-16 07:10:45 +0100

    update and extend RequestBuilder

--- src/app/in-memory-db/request/request-builder.ts
+++ src/app/in-memory-db/request/request-builder.ts
@@ -1,17 +1,30 @@
-import {IRequestCondition, IRequestField} from "citrus-common";
+import {IOrderDefinition, IRequestCondition, IRequestField} from "citrus-common";
 import {Request} from "./request";
 
 export class RequestBuilder {
   private includedFields: IRequestField[] = [];
   private conditions: IRequestCondition[] = [];
+  private order?: IOrderDefinition[];
+  private limit?: number;
+  private offset?: number;
 
   public constructor(private typeName: string) {
   }
 
   static createFromRequest(request: Request): RequestBuilder {
-    return new RequestBuilder(request.typeName)
+    const rb = new RequestBuilder(request.typeName)
       .addFields(request.includedFields)
       .addConditions(request.conditions);
+    if (!!request.order) {
+      rb.addOrder(request.order);
+    }
+    if (!!request.limit) {
+      rb.addLimit(request.limit);
+    }
+    if (!!request.offset) {
+      rb.addOffset(request.offset);
+    }
+    return rb;
   }
 
   public addFields(fields: IRequestField[]): RequestBuilder {
@@ -24,13 +37,28 @@ export class RequestBuilder {
     return this;
   }
 
-  public addCondition(condition: IRequestCondition) {
+  public addCondition(condition: IRequestCondition): RequestBuilder {
     this.conditions.push(condition);
     return this;
   }
 
-  public addConditions(conditions: IRequestCondition[]) {
+  public addConditions(conditions: IRequestCondition[]): RequestBuilder {
     this.conditions.push(...conditions);
+    return this;
+  }
+
+  public addOrder(order: IOrderDefinition[]): RequestBuilder {
+    this.order.push(...order);
+    return this;
+  }
+
+  public addLimit(limit: number): RequestBuilder {
+    this.limit = limit;
+    return this;
+  }
+
+  public addOffset(offset: number): RequestBuilder {
+    this.offset = offset;
     return this;
   }
 
