186f53d3ef37ea5122363b92345cc02a011f319a
David Leuenberger <david.leuenberger@hsr.ch>
2018-05-27 23:15:10 +0200

    combine users and address

--- src/app/childs/address/address-edit/address-edit.component.ts
+++ src/app/childs/address/address-edit/address-edit.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
-import {AddressDto} from "citrus-common";
+import {AddressDto, UserInfoDto} from "citrus-common";
+import "rxjs/add/observable/combineLatest";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Observable} from "rxjs/Observable";
+import {UserInfoDtoRestService} from "../../user/user-info-dto-rest.service";
 import {AddressDtoRestService} from "../address-dto-rest.service";
 
 @Component({
@@ -13,31 +17,45 @@ export class AddressEditComponent implements OnInit {
   public address: AddressDto = AddressDto.createEmpty();
   public addressID: number;
 
+  public userInfoSubject: BehaviorSubject<UserInfoDto[]> = new BehaviorSubject([]);
+
   constructor(private route: ActivatedRoute,
               private router: Router,
-              private rest: AddressDtoRestService) {
+              private rest: AddressDtoRestService,
+              public userInfoRest: UserInfoDtoRestService) {
   }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       if (params["id"] == null) {
-        this.address = AddressDto.createEmpty();
         this.addressID = this.address.id;
       } else {
-        this.rest.get(+params["id"])
-          .subscribe(
-            t => {
-              this.address = AddressDto.createAddressWithId(t.id, t);
-              this.addressID = this.address.id;
-            },
-            err => {
-              console.log(`Could not get address with id ${params["id"]} with error: ${err}`);
-            });
+        const userObservable: Observable<UserInfoDto[]> = this.userInfoRest.getAll();
+        const addressObservable = this.rest.get(+params["id"]);
+        Observable.combineLatest(addressObservable, userObservable, (a, users) => {
+          const address = AddressDto.createAddressWithId(a.id, a);
+          this.userInfoSubject.next(users);
+          for (const user of users) {
+            if ((address.user != null && address.user.id === user.id) ||
+              (address.userId === user.id)) {
+              address.user = user;
+            }
+          }
+          return address;
+        }).subscribe(
+          t => {
+            this.address = t;
+            this.addressID = this.address.id;
+          },
+          err => {
+            console.log(`Could not get address with id ${params["id"]} with error: ${err}`);
+          });
       }
     });
   }
 
   public submit() {
+    this.address.userId = this.address.user.id;
     if (this.addressID == null) {
       this.rest.add(new AddressDto(this.address))
         .subscribe(
