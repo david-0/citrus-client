1def71e9b981aa129d27b5a54b85bd4680352252
David Leuenberger <david.leuenberger@hsr.ch>
2018-06-04 07:09:38 +0200

    add Dockerfile

--- README.md
+++ README.md
@@ -26,3 +26,8 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+docker build -t citrus-client .
+
+docker run -it   -v ${PWD}:/usr/src/app   -v /usr/src/app/node_modules   -p 4200:4200   --rm   citrus-client 
+docker run -d   -v ${PWD}:/usr/src/app   -v /usr/src/app/node_modules   -p 4200:4200   --rm --name citrus-client-container citrus-client
--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -28,8 +27,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'PhantomJS', 'Chrome' ],
-//    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+      }
+    },
     singleRun: false,
   });
 };
