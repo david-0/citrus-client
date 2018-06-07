c79318becdfe0abbc8a0a3309ca07d6bd963a0db
David Leuenberger <david.leuenberger@hsr.ch>
2018-06-07 06:52:07 +0200

    update common, add pickupLocation Services

--- src/app/childs/pickup-location/pickup-location-dto.service.ts
+++ src/app/childs/pickup-location/pickup-location-dto.service.ts
@@ -1,10 +1,14 @@
+import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {PickupLocationDto} from "citrus-common/lib/dto/pickup-location-dto";
+import {GenericRestService} from "../../table-support/generic-rest.service";
+import {RestUrlPrefixService} from "../../table-support/rest-url-prefix.service";
 
 @Injectable({
   providedIn: "root"
 })
-export class PickupLocationDtoService {
-
-  constructor() {
+export class PickupLocationDtoService extends GenericRestService<PickupLocationDto> {
+  constructor(http: HttpClient, private restUrlPrefix: RestUrlPrefixService) {
+    super(http, restUrlPrefix.getApiRestPrefix() + "/pickupLocation");
   }
 }
