e1a9f9ed603da5a9fb535c60786e6e7684d2f06d
David Leuenberger <david.leuenberger@hsr.ch>
2018-06-23 19:04:38 +0200

    measurement redesign

--- src/app/childs/unit-of-measurement/unit-of-measurement-overview/unit-of-measurement-overview.component.ts
+++ src/app/childs/unit-of-measurement/unit-of-measurement-overview/unit-of-measurement-overview.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ["./unit-of-measurement-overview.component.scss"]
 })
 export class UnitOfMeasurementOverviewComponent implements OnInit {
-  public displayedColumns = ["id", "shortcut", "description"];
+  public displayedColumns = ["shortcut", "description"];
 
   constructor() {
   }
