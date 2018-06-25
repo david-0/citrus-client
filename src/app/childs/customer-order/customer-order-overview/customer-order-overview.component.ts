7e7d7003adeacefff321496841207762fcb556f6
David Leuenberger <david.leuenberger@hsr.ch>
2018-06-25 07:06:45 +0200

    show bestellnummer

--- src/app/childs/customer-order/customer-order-overview/customer-order-overview.component.ts
+++ src/app/childs/customer-order/customer-order-overview/customer-order-overview.component.ts
@@ -10,7 +10,7 @@ import {CustomerOrderDtoRestService} from "../customer-order-dto-rest.service";
 })
 export class CustomerOrderOverviewComponent implements OnInit {
 
-  public displayedColumns = ["date", "totalPrice", "pickupLocation", "user"];
+  public displayedColumns = ["id", "date", "totalPrice", "pickupLocation", "user"];
   public dataObservable = new BehaviorSubject<CustomerOrderDto[]>([]);
 
   constructor(private rest: CustomerOrderDtoRestService) {
