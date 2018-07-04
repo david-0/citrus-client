49dd476f17971cc147ba205f7a17ee65efa6764b
David Leuenberger <david.leuenberger@hsr.ch>
2018-07-04 17:52:42 +0200

    add totalPrice in Cart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {CartEntry} from "../cart/cart-entry";
 import {CartService} from "../cart/cart.service";
 
 @Component({
@@ -10,14 +11,28 @@ export class DashboardComponent implements OnInit {
 
   events: string[] = [];
   opened = true;
+  private _totalPrice = 0;
 
   constructor(public cartService: CartService) {
   }
 
+  get totalPrice(): number {
+    return this._totalPrice;
+  }
+
   ngOnInit() {
+    this.cartService.getCart().subscribe(cartEntries => {
+      this._totalPrice = this.computeTotalPrice(cartEntries);
+    });
   }
 
   clearCart() {
     this.cartService.clear();
+  }
+
+  private computeTotalPrice(cartEntries: CartEntry[]): number {
+    return cartEntries
+      .map(entry => +entry.count * +entry.price)
+      .reduce((a, b) => a + b, 0);
   }
 }
