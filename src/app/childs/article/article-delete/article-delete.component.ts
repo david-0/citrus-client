8090805b38276909bebe482046e6051fc7738ec0
David Leuenberger <david.leuenberger@hsr.ch>
2018-07-13 07:53:19 +0200

    add check on address delete that no Customer- and Vendororders belongs to this article

--- src/app/childs/article/article-delete/article-delete.component.ts
+++ src/app/childs/article/article-delete/article-delete.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
 import {ArticleDto} from "citrus-common";
 import {DeleteExecutor} from "../../../base/delete-executor";
-import {ArticleDtoRestService} from "../article-dto-rest.service";
+import {ArticleWithAllDtoRestService} from "../article-with-all-dto-rest.service";
 
 @Component({
   selector: "app-article-delete",
@@ -14,11 +14,27 @@ export class ArticleDeleteComponent implements OnInit {
   public deleteExecutor: DeleteExecutor<ArticleDto>;
 
   constructor(private route: ActivatedRoute,
-              private rest: ArticleDtoRestService) {
+              private rest: ArticleWithAllDtoRestService) {
   }
 
   ngOnInit() {
     this.deleteExecutor = new DeleteExecutor<ArticleDto>(this.route, this.rest, "Der Artikel");
+    this.deleteExecutor.registerCheck(article => this.getCustomerOrderIds(article).length > 0,
+      article => `weil er noch in ${this.getVendorOrderIds(article).length} Bestellung(en) verwendet wird`);
     this.deleteExecutor.initDelete();
+  }
+
+  private getCustomerOrderIds(article: ArticleDto): number[] {
+    return this.uniq(article.customerOrderItems.map(item => item.customerOrderId));
+  }
+
+  private getVendorOrderIds(article: ArticleDto): number[] {
+    return this.uniq(article.customerOrderItems.map(item => item.customerOrderId));
+  }
+
+  private uniq(a: number[]): number[] {
+    return a.sort().filter(function (item, pos, ary) {
+      return !pos || item !== ary[pos - 1];
+    });
   }
 }
