5547a0cc7776e506ac513edd1e9d74aa8d0328d1
David Leuenberger <david.leuenberger@hsr.ch>
2018-10-14 17:35:55 +0200

    add done flag in the UI

--- src/app/childs/article-checkin/article-checkin-overview/article-checkin-overview.component.ts
+++ src/app/childs/article-checkin/article-checkin-overview/article-checkin-overview.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from "@angular/core";
   styleUrls: ["./article-checkin-overview.component.scss"]
 })
 export class ArticleCheckinOverviewComponent implements OnInit {
-  public displayedColumns = ["articleStock", "quantity", "date", "comment", "user"];
+  public displayedColumns = ["articleStock", "quantity", "date", "done", "comment", "user"];
 
   constructor() {
   }
