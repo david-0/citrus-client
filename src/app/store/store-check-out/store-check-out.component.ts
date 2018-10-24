34cc7e92094d75763210d114f1472bf5674e1868
David Leuenberger <david.leuenberger@hsr.ch>
2018-10-24 08:01:00 +0200

    add store-check-out

--- src/app/store/store-check-out/store-check-out.component.ts
+++ src/app/store/store-check-out/store-check-out.component.ts
@@ -1,4 +1,9 @@
+import {HttpErrorResponse} from "@angular/common/http";
 import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ArticleCheckOutDto, ArticleStockDto} from "citrus-common";
+import {ArticleCheckoutWithAllDtoRestService} from "../../childs/article-checkout/article-checkout-with-all-dto-rest.service";
+import {ArticleStockWithDtoAllRestService} from "../../childs/article-stock/article-stock-with-dto-all-rest.service";
 
 @Component({
   selector: "app-store-check-out",
@@ -7,10 +12,27 @@ import {Component, OnInit} from "@angular/core";
 })
 export class StoreCheckOutComponent implements OnInit {
 
-  constructor() {
+  public articleCheckOut: ArticleCheckOutDto = ArticleCheckOutDto.createEmpty();
+  public articleStocks: ArticleStockDto[] = [];
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private articleCheckOutRest: ArticleCheckoutWithAllDtoRestService,
+              public articleStockRest: ArticleStockWithDtoAllRestService) {
   }
 
   ngOnInit() {
+    this.articleStockRest.getAll().subscribe(articles => {
+      this.articleStocks = articles;
+    });
   }
 
+  public submit() {
+    this.articleCheckOut.done = true;
+    this.articleCheckOutRest.add(this.articleCheckOut)
+      .subscribe(
+        (result) => this.router.navigate(["../.."], {relativeTo: this.route}),
+        (err: HttpErrorResponse) => console.error(`could not save articleStock: ${this.articleCheckOut.id} with Error: ${err.message}`)
+      );
+  }
 }
