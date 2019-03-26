cfd0f813a81f11b54bec2ba5fea2a90a1240e568
David Leuenberger <david.leuenberger@gmx.ch>
2019-03-26 08:04:19 +0100

    refactor

--- src/app/dashboard/public-article-stock-grid2/public-article-stock-grid2.component.ts
+++ src/app/dashboard/public-article-stock-grid2/public-article-stock-grid2.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {ArticleDto} from "citrus-common";
 import {CartService} from "../../cart/cart.service";
 import {ArticleInSaleDtoRestService} from "../article-in-sale--dto-rest.service";
 import {ArticleStockWrapper} from "../ArticleStockWrapper";
@@ -11,35 +12,23 @@ import {ArticleWrapper} from "../ArticleWrapper";
 })
 export class PublicArticleStockGrid2Component implements OnInit {
 
-  private _articlesWrappers: ArticleWrapper[];
+  private _articles: ArticleDto[];
 
   constructor(private rest: ArticleInSaleDtoRestService, private cartService: CartService) {
   }
 
-  public get articlesWrappers(): ArticleWrapper[] {
-    return this._articlesWrappers;
+  public get articles(): ArticleDto[] {
+    return this._articles;
   }
 
   ngOnInit() {
     this.rest.getAll().subscribe((articles) => {
-      this._articlesWrappers = articles
+      this._articles = articles
         .sort((a, b) => a.description.localeCompare(b.description))
-        .filter(a => a.articleStocks.length > 0)
-        .map(a => new ArticleWrapper(a));
-      this._articlesWrappers.forEach(a => {
-        a.articleStockWrappers.forEach(s => {
-          const cartItem = this.cartService.getOrderItem(s.articleStock.location.id, s.articleStock.article.id);
-          if (cartItem) {
-            s.cartItem = cartItem;
-          }
-        });
+        .filter(a => a.articleStocks.length > 0);
+      this._articles.forEach(a => {
+        a.articleStocks = a.articleStocks.sort((a, b) => a.location.description.localeCompare(b.location.description));
       });
     });
-  }
-
-  addToCart(articleStockWrapper: ArticleStockWrapper) {
-    this.cartService.addArticle(articleStockWrapper.articleStock.location, articleStockWrapper.articleStock.article, 1);
-    articleStockWrapper.cartItem = this.cartService.getOrderItem(
-      articleStockWrapper.articleStock.location.id, articleStockWrapper.articleStock.article.id);
   }
 }
