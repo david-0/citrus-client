c09ead1164632b90f3c0fcae9c3389d720e167b5
David <david.leuenberger@gmx.ch>
2022-11-08 00:30:48 +0100

    show only locations with a least one article in the webshop

---
+++ src/app/childs/location/location-with-openingh-hours-stocks-and-article-dto-rest.service.ts
@@ -0,0 +1,13 @@
+import {HttpClient} from "@angular/common/http";
+import {Inject, Injectable} from "@angular/core";
+import {LocationDto} from "citrus-common";
+import {GenericRestService} from "../../table-support/generic-rest.service";
+
+@Injectable({
+  providedIn: "root"
+})
+export class LocationWithOpeninghHoursStocksAndArticleDtoRestService extends GenericRestService<LocationDto> {
+  constructor(http: HttpClient, @Inject("baseUrl") baseUrl: string) {
+    super(http, baseUrl + "/location/withOpeningHoursStocksAndArticle");
+  }
+}
--- src/app/footer/location-opening-hours/location-opening-hours.component.ts
+++ src/app/footer/location-opening-hours/location-opening-hours.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationDto } from 'citrus-common';
 import { BehaviorSubject } from 'rxjs';
-import { LocationWithOpeninghHoursDtoRestService } from '../../childs/location/location-with-openingh-hours-dto-rest.service';
+import { LocationWithOpeninghHoursStocksAndArticleDtoRestService } from '../../childs/location/location-with-openingh-hours-stocks-and-article-dto-rest.service';
 
 @Component({
   selector: 'app-location-opening-hours',
@@ -13,13 +13,18 @@ export class LocationOpeningHoursComponent implements OnInit {
   public locations = new BehaviorSubject<LocationDto[]>([]);
 
 
-  constructor(private rest: LocationWithOpeninghHoursDtoRestService) {
+  constructor(private rest: LocationWithOpeninghHoursStocksAndArticleDtoRestService) {
   }
 
   ngOnInit() {
-    const subscription = this.rest.getAll().subscribe(data => {
-      this.locations.next(data);
-    });
-  }
-
+    const subscription = this.rest.getAll().subscribe(locations => {
+      const visibleLocations = locations //
+        .filter(location => {
+          return location.articleStocks //
+            .filter(stock => stock.article.inSale && stock.visible)
+            .length > 0;
+        });
+      this.locations.next(visibleLocations);
+    })
+  };
 }
